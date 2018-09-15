0133ef300058b0e976d0a23a832d35d117bb2b7e
ricky_gland821_0714@yahoo.co.jp <ricky_gland821_0714@yahoo.co.jp>
2018-09-15 17:33:29 +0900

    コントローラに画像作成ロジックを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
-  end
+  before_action :set_post, only: [:confirm, :edit, :update]
+  before_action :new_post, only: [:show, :new]
 
   # GET /posts/1
   # GET /posts/1.json
@@ -14,7 +9,6 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
   end
 
   # GET /posts/1/edit
@@ -26,39 +20,31 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if Post.last.present?
+      next_id = Post.last.id + 1
+    else
+      next_id = 1
+    end
+    make_picture(next_id)
+    if @post.save
+      redirect_to confirm_path(@post)
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+    if @post.update(post_params)
+      make_picture(@post.id)
+      redirect_to confirm_path(@post)
       else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def confirm
   end
 
   private
@@ -67,8 +53,93 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
 
+    def new_post
+      @post = Post.new
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:content, :picture, :kind)
     end
+    #画像を作成するためのメソット
+    def make_picture(id)
+      sentense = ""
+      content = @post.content.gsub(/\r\n|\r|\n/,"")
+      #文字数によって文字を設定
+      if content.length < 28 then
+        n = (content.length / 7).floor + 1
+        n.times do |i|
+          s_num = i*7
+          f_num = s_num + 6
+          range = Range.new(s_num, f_num)
+          sentense += content.slice(range)
+          sentense + = "\n" if n != i+ 1
+        end
+      pointsize = 90
+
+      elsif content.length <= 50 then
+        n = (content.length / 10).floor + 1
+        n.times do |i|
+          s_num = i * 10
+          f_num = s_num + 9
+          range =  Range.new(s_num,f_num)
+          sentense += content.slice(range)
+          sentense += "\n" if n != i+1
+        end
+      pointsize = 60
+
+      else
+        n = (content.length / 15).floor + 1
+        n.times do |i|
+          s_num = i * 15
+          f_num = s_num + 14
+          range =  Range.new(s_num,f_num)
+          sentense += content.slice(range)
+          sentense += "\n" if n != i+1
+        end
+      pointsize = 45
+      end
+      color = "black"
+      draw = "text 0,0 '#{sentense}'"
+      font = ".fonts/GenEiGothicN-U-KL.otf"
+
+      case @post.kind
+      when "black" then
+        base = base = "app/assets/images/black.jpg"
+      else
+        base = "app/assets/images/red.jp"
+      end
+      image = MiniMagik::Image.open(base)
+      image.combine_options do |i|
+        i.font font
+        i.fill color
+        i.gravity 'center'
+        i.pointsize pointsize
+        i.draw draw
+      end
+
+      storage = Fog::Storage.new(
+        provider: 'AWS',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: 'ap-northeast-1'
+      )
+      case Rails.env
+      when 'production'
+        # ⑨-14 バケットの指定・URLの設定
+        bucket = storage.directories.get('[アプリ名]-production')
+        # ⑨-15 保存するディレクトリ、ファイル名の指定（ファイル名は投稿id.pngとしています）
+        png_path = 'images/' + id.to_s + '.png'
+        image_uri = image.path
+        file = bucket.files.create(key: png_path, public: true, body: open(image_uri))
+        @post.picture = 'https://s3-ap-northeast-1.amazonaws.com/[アプリ名]-production' + "/" + png_path
+      when 'development'
+        bucket = storage.directories.get('[アプリ名]-development')
+        png_path = 'images/' + id.to_s + '.png'
+        image_uri = image.path
+        file = bucket.files.create(key: png_path, public: true, body: open(image_uri))
+        @post.picture = 'https://s3-ap-northeast-1.amazonaws.com/[アプリ名]-development' + "/" + png_path
+      end
+    end
+
 end
