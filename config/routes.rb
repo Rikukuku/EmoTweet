cb2eab9fb103b39d37e4b13fd38babf8f6165839
ricky_gland821_0714@yahoo.co.jp <ricky_gland821_0714@yahoo.co.jp>
2018-09-19 22:43:26 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #confirmの追加
+  get '/confirm/:id', to:'posts#confirm', as: :confirm
+  resources :posts, only: [:create, :show, :edit, :update]
+  root to: "posts#new"
+
 end
