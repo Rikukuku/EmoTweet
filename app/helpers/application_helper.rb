8bfd56aa87d12a399e41c8adb51ca20f0b917f8b
ricky_gland821_0714@yahoo.co.jp <ricky_gland821_0714@yahoo.co.jp>
2018-09-20 00:31:08 +0900

    実機テスト

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,8 @@ module ApplicationHelper
     if post.present?
       if post.id.present?
         twitter_card[:url] = "https://emotionaltweet.herokuapp.com/posts/#{post.id}"
-        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/emotionaltweet-production/images/#{post.id}.png"
+        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/emotionaltweet-production/images/22.png"
+        #twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/emotionaltweet-production/images/#{post.id}.png"
       else
         twitter_card[:url] = 'https://emotionaltweet.herokuapp.com/'
         twitter_card[:image] = "https://raw.githubusercontent.com/Rikukuku/emotionaltweet/master/app/assets/images/top.png"
@@ -14,7 +15,7 @@ module ApplicationHelper
       twitter_card[:image] = "https://raw.githubusercontent.com/Rikukuku/emotionaltweet/master/app/assets/images/top.png"
     end
     twitter_card[:title] = "エモツイ"
-    twitter_card[:card] = 'summary'
+    twitter_card[:card] = 'summary_large_image'
     twitter_card[:description] = '登録不要!感情をのせまくろう'
     twitter_card
   end
